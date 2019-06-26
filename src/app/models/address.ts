eb483b5625dad276fb2bc173569d1cfc5b962cf4
Eduardo Hernandorena <eduuardobh@gmail.com>
2019-06-26 15:41:09 -0300

    Inicio do formulario de pf

---
+++ src/app/models/address.ts
@@ -0,0 +1,9 @@
+export class Address {
+    street: string = '';
+    number: number;
+    complement: string = '';
+    district: string = '';
+    cep: string = '';
+    state: string = '';
+    city: string = '';
+}
---
+++ src/app/models/person.ts
@@ -0,0 +1,11 @@
+import { Address } from './address';
+
+export class Person {
+    name: string = '';
+    cfp: string = '';
+    active: string = 'ATIVO';
+    email: string = '';
+    start: Date = null;
+    address: Address = null;
+    phone: string = '';
+}
